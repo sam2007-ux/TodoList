ea6ddcd57b98bbc88b8f2725e8a21f1fd4d2502b
Samuel Yilma <yilmasamy@gmail.com>
2024-02-25 14:53:19 -0500

    Add more functionalitys
    Added methods, viewAllTasks(), viewCompletedTasks(), markCompletedTasks(), and sortTasks()

--- TodoList.java
+++ TodoList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TodoList {
   private ArrayList<String> tasks;
@@ -23,19 +24,52 @@ public class TodoList {
     }
   }
 
-  // view Task ---
-  public void ViewTasks(String task) {
+  // Imporved View All Tasks
+
+  public void viewAllTasks() {
     if (tasks.isEmpty()) {
-      System.out.println("No tasks in the list.");
+      System.out.println("No tasks in the list");
     } else {
+      System.out.println("----- All Tasks -----");
       for (int i = 0; i < tasks.size(); i++) {
         System.out.println((i + 1) + "." + tasks.get(i));
       }
     }
   }
 
-  // dummy print
-  public void print() {
-    System.out.println("Greetings!!");
+  // View Completed Tasks
+  public void viewCompletedTasks() {
+    ArrayList<String> completedTasks = new ArrayList<>();
+    for (String task : tasks) {
+      if (task.endsWith("[Completed]")) {
+        completedTasks.add(task);
+      }
+    }
+
+    if (completedTasks.isEmpty()) {
+      System.out.println("No completed tasks in the list.");
+    } else {
+      System.out.println("----- Completed Tasks -----");
+      for (int i = 0; i < completedTasks.size(); i++) {
+        System.out.println((i + 1) + "." + completedTasks.get(i));
+      }
+    }
   }
+
+  // Mark Task as completed
+  public void markTaskAsCompleted(String task) {
+    if (tasks.contains(task)) {
+      tasks.set(tasks.indexOf(task), task + "[Completed]");
+      System.out.println("Task \"" + task + "\" marked as completed.");
+    } else {
+      System.out.println("Task \"" + task + "\" not found in the list.");
+    }
+  }
+
+  // Sort Tasks
+  public void sortTasks() {
+    Collections.sort(tasks);
+    System.out.println("Tasks sorted Alphabetically");
+  }
+
 }
--- TodoListApp.java
+++ TodoListApp.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+//things to add, google like checks,(ex. if person types add tasks, it will think user was trying to type "add task" and will ask confimation if yes - proceed, no - try again)
+//incorporate menu function
+//make a setting (SortTaskA / sortTasksByDate)
 public class TodoListApp {
   public static Scanner scanner = new Scanner(System.in);
   public static TodoList todoList = new TodoList();
@@ -14,7 +17,10 @@ public class TodoListApp {
       System.out.println("\n--- To-Do List Application --- \n");
       System.out.println("     > Add Task");
       System.out.println("     > Remove Task");
-      System.out.println("     > View Tasks");
+      System.out.println("     > View All Tasks");
+      System.out.println("     > View Completed Tasks");
+      System.out.println("     > Mark Tasks as Completed");
+      System.out.println("     > Sort Tasks");
       System.out.println("     > Exit" + "\n");
 
       System.out.print("Enter your choice: ");
@@ -38,19 +44,36 @@ public class TodoListApp {
           String taskToRemove = scanner.nextLine();
           System.out.println();
           todoList.removeTasks(taskToRemove);
+          // things to add: for remove function, make tasks to remove visible
+          // TODO: implement making tasks to remove visible
           break;
-        case "view task":
-        case "vt":
+        case "view all tasks":
+        case "vat":
         case "3":
+          todoList.viewAllTasks();
+          break;
+        case "view completed tasks":
+        case "vct":
+        case "4":
+          todoList.viewCompletedTasks();
+          break;
+        case "mark task as completed":
+        case "mtac":
+        case "5":
           System.out.println();
-          System.out.print("Enter the Task to View : ");
-          String taskToView = scanner.nextLine();
+          System.out.print("Enter the Task to Mark as Completed: ");
+          String taskToMark = scanner.nextLine();
           System.out.println();
-          todoList.ViewTasks(taskToView);
+          todoList.markTaskAsCompleted(taskToMark);
+          break;
+        case "sort tasks":
+        case "st":
+        case "6":
+          todoList.sortTasks();
           break;
         case "exit":
         case "e":
-        case "4":
+        case "7":
           System.out.println();
           System.out.println("Exiting the To-Do List Application. Goodbye!");
           scanner.close();
